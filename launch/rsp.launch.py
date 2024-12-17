d7993414a19e2ad33e767453f11a5113f550e80d
MOhammedEmir <moemir559@gmail.com>
2024-12-17 04:37:05 +0300

    created core URDF

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -1,41 +1,72 @@
 import os
 
-from ament_index_python.packages import get_package_share_directory
-
-from launch import LaunchDescription
-from launch.substitutions import LaunchConfiguration
-from launch.actions import DeclareLaunchArgument
-from launch_ros.actions import Node
-
+import launch
 import xacro
 
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition, UnlessCondition
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
+    beetle_desc_dir = FindPackageShare(package='arac_proj').find('arac_proj')
+    robot_description = xacro.process_file(os.path.join(beetle_desc_dir, 'description/robot.urdf.xacro')).toxml()
+    default_rviz_config_file = os.path.join(beetle_desc_dir, 'rviz/default_view.rviz')
 
-    # Check if we're told to use sim time
-    use_sim_time = LaunchConfiguration('use_sim_time')
+    # Create launch configuration variables
+    use_js_pub_gui = LaunchConfiguration('use_js_pub_gui')
+    rviz_config_file = LaunchConfiguration('rviz_config_file')
 
-    # Process the URDF file
-    pkg_path = os.path.join(get_package_share_directory('arac_proj'))
-    xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
-    robot_description_config = xacro.process_file(xacro_file)
-    
-    # Create a robot_state_publisher node
-    params = {'robot_description': robot_description_config.toxml(), 'use_sim_time': use_sim_time}
-    node_robot_state_publisher = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        output='screen',
-        parameters=[params]
+    # Define launch arguments
+    declare_use_js_pub_gui_cmd = DeclareLaunchArgument(
+        name='use_js_pub_gui', default_value='True',
+        description='Flag to enable joint_state_publisher_gui'
+    )
+    declare_rviz_config_file_cmd = DeclareLaunchArgument(
+        name='rviz_config_file', default_value=default_rviz_config_file,
+        description='Absolute path to rviz config file'
     )
 
+    # Define actions
+    start_joint_state_publisher = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher',
+        condition=UnlessCondition(use_js_pub_gui)
+    )
+    start_joint_state_publisher_gui = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        name='joint_state_publisher_gui',
+        condition=IfCondition(use_js_pub_gui)
+    )
+    start_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{'robot_description': robot_description}]
+    )
+    start_rviz = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', rviz_config_file],
+    )
 
-    # Launch!
-    return LaunchDescription([
-        DeclareLaunchArgument(
-            'use_sim_time',
-            default_value='false',
-            description='Use sim time if true'),
+    # Create launch description
+    ld = launch.LaunchDescription()
 
-        node_robot_state_publisher
-    ])
+    # Declare launch options
+    ld.add_action(declare_use_js_pub_gui_cmd)
+    ld.add_action(declare_rviz_config_file_cmd)
+
+    # Add conditioned actions
+    ld.add_action(start_joint_state_publisher)
+    ld.add_action(start_joint_state_publisher_gui)
+
+    # Add actions
+    ld.add_action(start_robot_state_publisher)
+    ld.add_action(start_rviz)
+
+    return ld
